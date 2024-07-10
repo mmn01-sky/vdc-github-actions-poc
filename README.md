d8d611828bd46fc69b184c7d5ca1337f5641cc47
mmn01 <moustafa.mneimneih@sky.uk>
2024-07-10 20:06:36 +0100

    VDCTEAM-6290 [MM] :: Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # vdc-github-actions-poc
+
+
+setup similar actions concourse
+setup self hosted runner
+set environment
+set environment variables
+set enviroment secrets
+create another project that shares runner
+
+# notes
+created vdc project Mous in dev
+created rocky linux Corp Lite 8vcpu 8192memory
---
+++ projects/python-demo/tests/test_main.py
@@ -0,0 +1,21 @@
+# test_simple_functions.py
+import pytest
+from main import add, subtract, multiply
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+    assert subtract(1, 2) == -1
+    assert subtract(0, 0) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 1) == 0
+
+if __name__ == "__main__":
+    pytest.main()
