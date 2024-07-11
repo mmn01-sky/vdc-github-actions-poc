8989d726edc660dd5ddf0e0b919ca8c3ad692af3
mmn01 <moustafa.mneimneih@sky.uk>
2024-07-11 08:27:35 +0100

    VDCTEAM-6290 [MM] :: Updated Dummy TF

--- projects/terraform-demo/main.tf
+++ projects/terraform-demo/main.tf
@@ -17,10 +17,20 @@ output "dummy_output" {
 
 resource "null_resource" "dummy2" {
   provisioner "local-exec" {
-    command = "echo Hello, Terraform!"
+    command = "echo Hello, Terraform2!"
   }
 }
 
 output "dummy_output2" {
   value = "This is dummy output 2"
+}
+
+resource "null_resource" "dummy3" {
+  provisioner "local-exec" {
+    command = "echo Hello, Terraform3!"
+  }
+}
+
+output "dummy_output3" {
+  value = "This is dummy output 3"
 }
