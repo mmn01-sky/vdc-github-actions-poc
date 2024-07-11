db5b4ef3e1625ed55bc353a4c179104f9e2ca1ac
mmn01 <moustafa.mneimneih@sky.uk>
2024-07-11 12:48:39 +0100

    VDCTEAM-6290 [MM] :: Initial Commit

--- projects/terraform-demo/main.tf
+++ projects/terraform-demo/main.tf
@@ -24,13 +24,3 @@ resource "null_resource" "dummy2" {
 output "dummy_output2" {
   value = "This is dummy output 2"
 }
-
-resource "null_resource" "dummy3" {
-  provisioner "local-exec" {
-    command = "echo Hello, Terraform3 PR!"
-  }
-}
-
-output "dummy_output3" {
-  value = "This is dummy output 3"
-}
